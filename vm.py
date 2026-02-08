d7cf07998c9d888c368d63f6d217c23b19576d9c
Faizan Ahmad <faizanahmad2582001@gmail.com>
2026-02-08 19:27:57 +0500

    run_code function is done, working on make_frame and run_frame

--- vm.py
+++ vm.py
@@ -4,4 +4,19 @@ class VirtualMachineError(Exception):
 
 class VirtualMachine(object):
     def __init__(self) -> None:
-        pass
+        self.frame_call_stack = []
+        self.current_frame = None
+        self.returned_value = None
+        self.exception = None
+
+    def run_code(self, code_obj, global_names=None, local_names=None):
+        frame = self.make_frame(
+            code_obj, global_names=global_names, local_names=local_names
+        )
+
+        self.run_frame(frame)
+
+    def make_frame(self, code_obj, global_names, local_names):
+        return "None"
+
+    def run_frame(self, frame): ...
